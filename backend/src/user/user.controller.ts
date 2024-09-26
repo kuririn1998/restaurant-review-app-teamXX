da312847c16d2da35b3782a5ab495b9cb2d0aa30
kuririn1998 <rinrin.kuribayashi@gmail.com>
2024-09-26 16:19:39 +0900

    Added user registration feature

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('api')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+// frontend/pages/index.js
+
+const HomePage = () => {
+  return <h1>Welcome to the Restaurant Review App</h1>;
+};
+
+export default HomePage;
